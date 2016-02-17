40eae6a3a66d20d41c6b59c2d16f0f3bc864bcb1
napasa <1011364522@qq.com>
2016-02-17 23:32:49 +0800

    change three files and modify one

--- ICEPLAYER/aboutform.h
+++ ICEPLAYER/aboutform.h
@@ -2,22 +2,31 @@
 #define ABOUT_H
 
 #include <QDialog>
-#include <qwidgetresizehandler_p.h>
+//#include <qwidgetresizehandler_p.h>
 #include "iceButton.h"
+#include "movablewindow.h"
+class ICE_Ice_Button;
 
-class ICE_About_Form : public QDialog
+class ICE_About_Form : public movableWindow
 {
-	Q_OBJECT
+    Q_OBJECT
 public:
-	ICE_About_Form(QWidget *parent = 0);
-	~ICE_About_Form();
+    ICE_About_Form(QWidget *parent = 0);
+    ~ICE_About_Form();
 
 private:
 
-	//»æÖÆ±³¾°Í¼Æ¬
-	void paintEvent(QPaintEvent *event); 
+    //ï¿½ï¿½ï¿½Æ±ï¿½ï¿½ï¿½Í¼Æ¬
+    void paintEvent(QPaintEvent *event);
+   // void mousePressEvent(QMouseEvent *event);
+   // void mouseMoveEvent(QMouseEvent *event);
+   // void mouseReleaseEvent(QMouseEvent *event);
 
-	ICE_Ice_Button *exitButton;
+   // bool m_drag;
+   // bool m_move;
+  //  QPoint dragPos;
+  //  QRect mouseDownRect;
+    ICE_Ice_Button *exitButton;
 };
 
 
